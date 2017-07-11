bed181252ac0f6f71afb485ba711d5ce424ad31e
Vivian Dao <v-vydao@microsoft.com>
2017-07-11 11:13:49 -0700

    added level iteration, updated color sequence, and checking player input

--- SimonSays/GameStateManager.swift
+++ SimonSays/GameStateManager.swift
@@ -16,18 +16,34 @@ class GameStateManager {
     // If right, and no more plays left, level has succeeded
     
     
-    func checkPlayerInput(playerInput:ColorType) {
+    func checkPlayerInput(playerInput: ColorType) {
+        for check in gameState.sequence {
+            if playerInput == check {
+                gameState.status = .levelPassed
+            }
+            else {
+                gameState.status = .levelFailed
+                return
+            }
+        }
     }
+    
     
     // Game can only move to next level if: 
     // - game hasn't begun 
     // - current level has succeeded
     // GameState is updated when level increments
     func startNextLevel() {
+        gameState.currentLevel += 1
+        let newColor: ColorType = ColorType(rawValue: Int(arc4random_uniform(4)))!
+        gameState.sequence.append(newColor)
     }
 
     // A new game can be started at any time
     // Starting a game starts the first level of the game
     func startNewGame() {
+        gameState = GameState()
+        startNextLevel()
+        
     }
 }
