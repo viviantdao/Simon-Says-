5f2bcd0956615614004abfb29240bdc326d66e6b
Vivian Dao <v-vydao@microsoft.com>
2017-07-17 09:53:50 -0700

    finalized global & local high scores

--- SimonSays/HighScoreViewController.swift
+++ SimonSays/HighScoreViewController.swift
@@ -10,50 +10,81 @@ import UIKit
 
 class HighScoreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    //var highScoreArray: [StructOfHighScores]!
     var highScores = AppDelegate.highScoreManager.getScores()
-    // UITableViewDataSource data source means that this will be the one resposible for running the data to the table view
+    var globalHighScores = [StructOfHighScores]()
     
-    //by tomorrow, grab array off manager, return how big the array is and the number of rows and set the cells in detailed text label
     
+    @IBAction func playAgainButton(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    @IBOutlet weak var highScoreTable: UITableView!
+    
+  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.highScores.count
-        //we don't know how many rows we need
-        //could return 0 or 1
+        if section == 0 {
+         return self.highScores.count
+        }
+        else {
+        return self.globalHighScores.count
+        }
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "local high scores"
+        }
+        else {
+            return "global high scores"
+        }
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // we can use a struct for the section and row number 
+        // we can use a struct for the section and row number
         let cell = self.highScoreTable.dequeueReusableCell(withIdentifier: "HighScoreCell", for: indexPath)
-        
+        if indexPath.section == 0
+        {
         let highScoreAtIndex = self.highScores[indexPath.row]
         cell.textLabel?.text = highScoreAtIndex.name
         cell.detailTextLabel?.text = String(highScoreAtIndex.score)
+        }
+        else {
+            let globalHighScoreAtIndex = self.globalHighScores[indexPath.row]
+            cell.textLabel?.text = globalHighScoreAtIndex.name
+            cell.detailTextLabel?.text = String(globalHighScoreAtIndex.score)
+        }
         
         return cell
     }
-   
-    @IBAction func playAgainButton(_ sender: Any) {
-        self.navigationController?.popToRootViewController(animated: true)
-        
-    }
-    // underscore means you don't have to explicitly name it, you don't need to name the specific type of parameter
-
-    @IBOutlet weak var highScoreTable: UITableView!
     
-    
-// connect play again to the beginning of the screen
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
- 
+    override func viewWillAppear (_ animated: Bool) {
+        AppDelegate.highScoreManager.getGlobalScores { (globalHighScores) in
+            self.globalHighScores = globalHighScores
+            self.highScoreTable.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-  
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+// 
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 }
+// UITableViewDataSource data source means that this will be the one resposible for running the data to the table view
+// need to be table to determine what section it wants, and use indexPathRow
+
+// need to add another section to table view 
+// tell table view I have another section
+// tell table view the correct row count
+// pull data from the correct array to connect scores 
+//
